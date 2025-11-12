f5a96594bd7664b2d5a31f3841034aa306db8dda
JiaHaoLin69 <jiahaolinyt@gmail.com>
2025-11-12 10:39:55 +0100

    Checkeo de Usuario Y home

--- docker/caronte/dockerfiles/base/Admin/start.sh
+++ docker/caronte/dockerfiles/base/Admin/start.sh
@@ -14,13 +14,33 @@ checkusuario(){
     fi
 }
 
+checkhome(){
+    if [ ! -d "/home/${USUARIO}" ] 
+    then
+        echo "El directorio /home/${USUARIO} no existe." >> /root/logs/informe.log
+        return 0 # TRUE
+    else
+        echo "El directorio /home/${USUARIO} ya existe." >> /root/logs/informe.log
+        return 1 # FALSO
+    fi
+}
+
 newUser(){
     checkusuario
-    if [ "$?" -eq 0 ]
+    if [ "$?" -eq 0 ] # No existe el usuario y $? Recoge el valor de retorno del ultimo comando ejecutado
     then
+        checkhome
+        if [ "$?" -eq 0 ] # No existe el home
+        then
         useradd -rm -d /home/${USUARIO} -s  /bin/bash ${USUARIO} 
         echo "${USUARIO}:${PASSWORD}" | chpasswd
         echo "Bienvenido ${USUARIO} ..." > /home/${USUARIO}/bienvenida.txt
+        echo "---> Usuario ${USUARIO} creado correctamente." >> /root/logs/informe.log
+        else
+        echo "---> Usuario ${USUARIO} no creado. El home ya existe." >> /root/logs/informe.log
+        fi
+    else
+        echo "---> Usuario ${USUARIO} no creado. Ya existe en el sistema." >> /root/logs/informe.log
     fi
 
 }

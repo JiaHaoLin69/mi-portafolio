b8ca95202a415aff8240347dbe58be74f9177d48
JiaHaoLin69 <jiahaolinyt@gmail.com>
2025-11-17 13:14:44 +0100

    cambios

--- docker/caronte/dockerfiles/base/Admin/start.sh
+++ docker/caronte/dockerfiles/base/Admin/start.sh
@@ -6,10 +6,10 @@ source /root/admin/base/usuario/mainuser.sh
 configurar_ssh() {
 
   # 1. Deshabilitar el login de root
-  sed -i 's/^#?PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
+  sed -i 's/#?PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
 
   # 2. Cambiar el puerto de SSH
-  sed -i 's/^#?Port.*/Port 23456/' /etc/ssh/sshd_config
+  sed -i 's/#?Port.*/Port 23456/' /etc/ssh/sshd_config
 # 3. Configurar la autenticación por clave pública
   mkdir -p /var/run/sshd
   ssh-keygen -A

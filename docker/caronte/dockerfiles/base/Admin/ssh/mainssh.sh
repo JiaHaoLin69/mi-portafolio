6c1b1ef809f7c057561881ddb085a10c9cffec03
JiaHaoLin69 <jiahaolinyt@gmail.com>
2025-11-19 11:46:59 +0100

    keygen

--- docker/caronte/dockerfiles/base/Admin/ssh/mainssh.sh
+++ docker/caronte/dockerfiles/base/Admin/ssh/mainssh.sh
@@ -6,11 +6,11 @@ configurar_ssh() {
   sed -i 's/#PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
   # 2. Cambiar el puerto de SSH
   sed -i 's/#Port.*/Port '$PORT_SSH'/' /etc/ssh/sshd_config
-  mkdir /home/$USUARIO/.ssh
-  cp ./common/ >> /home/$USUARIO/.ssh/
-  #4 Reinicar el servicio SSH para que se aplique las configuraciones
+    #4 Reinicar el servicio SSH para que se aplique las configuraciones
   service ssh restart # ESTO DARÁ PROBLEMAS A FUTURO POR LO QUE USAREMOS EL QUE HAY COMENTADO ABAJO
   # /etc/init.d/ssh start
   # exec /usr/sbin/sshd -D & # dejar el ssh en background PARA CUANDO LO IMPLEMENTOS EN UN SERVICIO
+  mkdir /home/$USUARIO/.ssh
+  cp ./common/ >> /home/$USUARIO/.ssh/
 
 }

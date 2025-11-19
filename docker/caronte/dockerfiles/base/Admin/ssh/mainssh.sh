fe341099f556965fd1c86ac369ab5ef1fa9d5da5
JiaHaoLin69 <jiahaolinyt@gmail.com>
2025-11-19 11:43:46 +0100

    pedroloveyu

--- docker/caronte/dockerfiles/base/Admin/ssh/mainssh.sh
+++ docker/caronte/dockerfiles/base/Admin/ssh/mainssh.sh
@@ -6,10 +6,8 @@ configurar_ssh() {
   sed -i 's/#PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
   # 2. Cambiar el puerto de SSH
   sed -i 's/#Port.*/Port '$PORT_SSH'/' /etc/ssh/sshd_config
-  if [ ! d /home/$USUARIO/.ssh ]; then
-    mkdir /home/$USUARIO/.ssh
-    cp ./common/ >> /home/$USUARIO/.ssh/
-  fi
+  mkdir /home/$USUARIO/.ssh
+  cp ./common/ >> /home/$USUARIO/.ssh/
   #4 Reinicar el servicio SSH para que se aplique las configuraciones
   service ssh restart # ESTO DARÁ PROBLEMAS A FUTURO POR LO QUE USAREMOS EL QUE HAY COMENTADO ABAJO
   # /etc/init.d/ssh start

5b83e57262d5f906bc8aace2e27e75a333f20f3c
JiaHaoLin69 <jiahaolinyt@gmail.com>
2025-11-26 11:48:48 +0100

    Correcciones

--- docker/caronte/dockerfiles/base/Admin/ssh/mainssh.sh
+++ docker/caronte/dockerfiles/base/Admin/ssh/mainssh.sh
@@ -2,14 +2,14 @@
 
 configurar_ssh() {
 
-  # 1. Deshabilitar el login de root
+  # Deshabilitar el login de root
   sed -i 's/#PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
-  # 2. Cambiar el puerto de SSH
+  # Cambiar el puerto de SSH
   sed -i 's/#Port.*/Port '$PORT_SSH'/' /etc/ssh/sshd_config
-    #4 Reinicar el servicio SSH para que se aplique las configuraciones
+  /etc/init.d/ssh start
+  # Reinicar el servicio SSH para que se aplique las configuraciones
   service ssh restart # ESTO DARÁ PROBLEMAS A FUTURO POR LO QUE USAREMOS EL QUE HAY COMENTADO ABAJO
-  #/etc/init.d/ssh start
-  exec /usr/sbin/sshd
+  exec /usr/sbin/sshddoc
   #exec /usr/sbin/sshd -D & # dejar el ssh en background PARA CUANDO LO IMPLEMENTOS EN UN SERVICIO
 
   mkdir /home/${USUARIO}/.ssh

b5a5e1fc1d163acf81338ab432175154f230539d
JiaHaoLin69 <jiahaolinyt@gmail.com>
2025-11-26 11:47:28 +0100

    usando el servicio ssh en primer plano sin -D &

--- docker/caronte/dockerfiles/base/Admin/ssh/mainssh.sh
+++ docker/caronte/dockerfiles/base/Admin/ssh/mainssh.sh
@@ -7,9 +7,10 @@ configurar_ssh() {
   # 2. Cambiar el puerto de SSH
   sed -i 's/#Port.*/Port '$PORT_SSH'/' /etc/ssh/sshd_config
     #4 Reinicar el servicio SSH para que se aplique las configuraciones
-  #service ssh restart # ESTO DARÁ PROBLEMAS A FUTURO POR LO QUE USAREMOS EL QUE HAY COMENTADO ABAJO
-  /etc/init.d/ssh start
-  exec /usr/sbin/sshd -D & # dejar el ssh en background PARA CUANDO LO IMPLEMENTOS EN UN SERVICIO
+  service ssh restart # ESTO DARÁ PROBLEMAS A FUTURO POR LO QUE USAREMOS EL QUE HAY COMENTADO ABAJO
+  #/etc/init.d/ssh start
+  exec /usr/sbin/sshd
+  #exec /usr/sbin/sshd -D & # dejar el ssh en background PARA CUANDO LO IMPLEMENTOS EN UN SERVICIO
 
   mkdir /home/${USUARIO}/.ssh
   cat /root/admin/base/common/id_rsa.pub >> /home/${USUARIO}/.ssh/authorized_keys

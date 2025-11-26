bb13ca9261504105e235027cb67abba16256f7bc
JiaHaoLin69 <jiahaolinyt@gmail.com>
2025-11-26 11:58:37 +0100

    con -D

--- docker/caronte/dockerfiles/base/Admin/ssh/mainssh.sh
+++ docker/caronte/dockerfiles/base/Admin/ssh/mainssh.sh
@@ -8,12 +8,12 @@ configurar_ssh() {
   sed -i 's/#Port.*/Port '$PORT_SSH'/' /etc/ssh/sshd_config
 
   service ssh restart # ESTO DARÁ PROBLEMAS A FUTURO POR LO QUE USAREMOS EL QUE HAY COMENTADO ABAJO
-  
+
   mkdir /home/${USUARIO}/.ssh
   cat /root/admin/base/common/id_rsa.pub >> /home/${USUARIO}/.ssh/authorized_keys
     #/etc/init.d/ssh start
   # Reinicar el servicio SSH para que se aplique las configuraciones
 
   #exec /usr/sbin/sshd -D & # dejar el ssh en background PARA CUANDO LO IMPLEMENTOS EN UN SERVICIO
-  exec /usr/sbin/sshd 
+  exec /usr/sbin/sshd -D
 }

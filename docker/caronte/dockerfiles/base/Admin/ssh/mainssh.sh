92f3d1ee59ae5df245fea623c297724726d70e77
JiaHaoLin69 <jiahaolinyt@gmail.com>
2025-11-26 11:49:41 +0100

    quitamos el restart

--- docker/caronte/dockerfiles/base/Admin/ssh/mainssh.sh
+++ docker/caronte/dockerfiles/base/Admin/ssh/mainssh.sh
@@ -8,7 +8,7 @@ configurar_ssh() {
   sed -i 's/#Port.*/Port '$PORT_SSH'/' /etc/ssh/sshd_config
   /etc/init.d/ssh start
   # Reinicar el servicio SSH para que se aplique las configuraciones
-  service ssh restart # ESTO DARÁ PROBLEMAS A FUTURO POR LO QUE USAREMOS EL QUE HAY COMENTADO ABAJO
+  #service ssh restart # ESTO DARÁ PROBLEMAS A FUTURO POR LO QUE USAREMOS EL QUE HAY COMENTADO ABAJO
   exec /usr/sbin/sshd
   #exec /usr/sbin/sshd -D & # dejar el ssh en background PARA CUANDO LO IMPLEMENTOS EN UN SERVICIO
 

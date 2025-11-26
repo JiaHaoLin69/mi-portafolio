2a523398893ddda8660e78274b9c9d0f384eb898
JiaHaoLin69 <jiahaolinyt@gmail.com>
2025-11-26 11:45:57 +0100

    cambiamos el metodo de ejecución

--- docker/caronte/dockerfiles/base/Admin/ssh/mainssh.sh
+++ docker/caronte/dockerfiles/base/Admin/ssh/mainssh.sh
@@ -7,7 +7,7 @@ configurar_ssh() {
   # 2. Cambiar el puerto de SSH
   sed -i 's/#Port.*/Port '$PORT_SSH'/' /etc/ssh/sshd_config
     #4 Reinicar el servicio SSH para que se aplique las configuraciones
-  service ssh restart # ESTO DARÁ PROBLEMAS A FUTURO POR LO QUE USAREMOS EL QUE HAY COMENTADO ABAJO
+  #service ssh restart # ESTO DARÁ PROBLEMAS A FUTURO POR LO QUE USAREMOS EL QUE HAY COMENTADO ABAJO
   /etc/init.d/ssh start
   #exec /usr/sbin/sshd -D & # dejar el ssh en background PARA CUANDO LO IMPLEMENTOS EN UN SERVICIO
 

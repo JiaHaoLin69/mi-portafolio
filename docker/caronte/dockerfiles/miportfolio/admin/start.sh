3aff3a462a178aec0e7be0cd600cb62aa345fe46
JiaHaoLin69 <jiahaolinyt@gmail.com>
2025-12-03 15:40:20 +0100

    deberia

--- docker/caronte/dockerfiles/miportfolio/admin/start.sh
+++ docker/caronte/dockerfiles/miportfolio/admin/start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-load_entrypoint_ubbase(){
-    bash /root/admin/base/start.sh
-}
+
 load_entrypoint_nginx(){
     bash /root/admin/sweb/nginx/admin/start.sh
 }
@@ -9,8 +7,7 @@ reload_nginx(){
     service nginx reload
 }
 main(){
-    
-    load_entrypoint_ubbase
+
     load_entrypoint_nginx
     reload_nginx
     tail -f /dev/null

80465a6de80a849b3cd649732071e460b833183c
JiaHaoLin69 <jiahaolinyt@gmail.com>
2025-12-03 12:58:09 +0100

    FIN

--- docker/caronte/dockerfiles/miportfolio/admin/start.sh
+++ docker/caronte/dockerfiles/miportfolio/admin/start.sh
@@ -5,9 +5,13 @@ load_entrypoint_base(){
 load_entrypoint_nginx(){
     bash /root/admin/sweb/nginx/admin/start.sh
 }
+reload_nginx(){
+    service nginx reload
+}
 main(){
     load_entrypoint_base
     load_entrypoint_nginx
+    reload_nginx
     tail -f /dev/null
 }
 

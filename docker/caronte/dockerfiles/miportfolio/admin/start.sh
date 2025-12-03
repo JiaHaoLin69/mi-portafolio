0e2210346ae5ddb300124ea9d0d9a9d037e9ee86
JiaHaoLin69 <jiahaolinyt@gmail.com>
2025-12-03 15:29:18 +0100

    correcciones

--- docker/caronte/dockerfiles/miportfolio/admin/start.sh
+++ docker/caronte/dockerfiles/miportfolio/admin/start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-load_entrypoint_base(){
-    bash /root/admin/base/start.sh
-}
 load_entrypoint_nginx(){
     bash /root/admin/sweb/nginx/admin/start.sh
 }
@@ -9,7 +6,6 @@ reload_nginx(){
     service nginx reload
 }
 main(){
-    load_entrypoint_base
     load_entrypoint_nginx
     reload_nginx
     tail -f /dev/null

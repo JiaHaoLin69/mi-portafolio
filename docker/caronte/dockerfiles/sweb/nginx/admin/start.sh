01d4a7d2e94b1c4a48a52a8bf1b9986154fea8df
JiaHaoLin69 <jiahaolinyt@gmail.com>
2025-12-03 12:04:14 +0100

    Creación de la imagen Nginx

--- docker/caronte/dockerfiles/sweb/nginx/admin/start.sh
+++ docker/caronte/dockerfiles/sweb/nginx/admin/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 config_nginx() {
-    nginx 
+    nginx &
 }
 
 load_entrypoint_base(){
@@ -10,8 +10,6 @@ load_entrypoint_base(){
 main(){
  load_entrypoint_base
  config_nginx
- tail -f /dev/null
-
 }
 
 main

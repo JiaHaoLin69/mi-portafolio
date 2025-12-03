cb6bc62ccdb7347bdf974fcbe6079a390f3ea06a
JiaHaoLin69 <jiahaolinyt@gmail.com>
2025-12-03 15:54:27 +0100

    cambios en usuarios

--- docker/caronte/dockerfiles/base/Admin/usuario/mainuser.sh
+++ docker/caronte/dockerfiles/base/Admin/usuario/mainuser.sh
@@ -32,14 +32,14 @@ newUser(){
         useradd -rm -d /home/${USUARIO} -s  /bin/bash ${USUARIO} 
         echo "${USUARIO}:${PASSWORD}" | chpasswd
         echo "Bienvenido ${USUARIO} ..." > /home/${USUARIO}/bienvenida.txt
-        echo "---> Usuario ${USUARIO} creado correctamente." >> /root/logs/informe.log
+        #echo "---> Usuario ${USUARIO} creado correctamente." >> /root/logs/informe.log
         return 0
         else
-        echo "---> Usuario ${USUARIO} no creado. El home ya existe." >> /root/logs/informe.log
+        #echo "---> Usuario ${USUARIO} no creado. El home ya existe." >> /root/logs/informe.log
         return 1
         fi
     else
-        echo "---> Usuario ${USUARIO} no creado. Ya existe en el sistema." >> /root/logs/informe.log
+        #echo "---> Usuario ${USUARIO} no creado. Ya existe en el sistema." >> /root/logs/informe.log
         return 1
     fi
 

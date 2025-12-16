0b516d25d5d3f51095f6db9b3d724e22d5d396a8
JiaHaoLin69 <jiahaolinyt@gmail.com>
2025-12-16 12:18:33 +0100

    modificación en el nginx

--- docker/caronte/dockerfiles/sweb/nginx/admin/start.sh
+++ docker/caronte/dockerfiles/sweb/nginx/admin/start.sh
@@ -7,6 +7,18 @@ load_entrypoint_base(){
     bash /root/admin/base/start.sh
 }
 
+ajustar_nginx(){
+    server {
+    listen 80;
+
+    location / {
+
+        proxy_pass http://194.163.147.140:8888; 
+
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+    }
+
 main(){
  load_entrypoint_base
  config_nginx

21546331d8e4c4ff8277b5f9baa94666ac1b283d
JiaHaoLin69 <jiahaolinyt@gmail.com>
2026-01-21 12:37:19 +0100

    a

--- docker/caronte/dockerfiles/miportfolio/admin/start.sh
+++ docker/caronte/dockerfiles/miportfolio/admin/start.sh
@@ -4,13 +4,14 @@ load_entrypoint_nginx(){
     bash /root/admin/sweb/nginx/admin/start.sh
 }
 reload_nginx(){
+    nginx -t
     service nginx reload
+    nginx -g "daemon off;"
 }
 main(){
 
     load_entrypoint_nginx
     reload_nginx
-    tail -f /dev/null
 }
 
 main
